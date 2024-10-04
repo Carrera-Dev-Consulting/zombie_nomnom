fd793ef40f467575194287da01c8ce24addc9ed7
Andres H Carrera <andreshcar@live.com>
2024-10-04 00:32:39 -0600

    Making the app exectuble by moving things around (#7)

--- zombie_dice/__main__.py
+++ zombie_dice/__main__.py
@@ -1,24 +1,3 @@
-import click
-from .cli import run_game
-
-
-@click.group()
-def main():
-    pass
-
-
-@main.command("cli")
-def cli():
-    """
-    Command to start the zombie_dice game from the command line.
-    """
-    run_game()
-
-    # ask after we finish a single game assume they will quit when they want to.
-    while click.confirm(text="Play another game of zombie dice?"):
-        run_game()
-
-    click.echo("Thank you for playing!!")
-
+from .app import main
 
 main()
